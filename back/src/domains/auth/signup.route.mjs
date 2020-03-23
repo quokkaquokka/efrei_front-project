68c733d80f777a6d706f413bac3a52673228d3f8
Camille Moutte <mouttecam@gmail.com>
2020-03-23 18:08:25 +0100

    back: fix bug route signup

--- back/src/domains/auth/signup.route.mjs
+++ back/src/domains/auth/signup.route.mjs
@@ -5,21 +5,21 @@ import { addUserIfNotExists } from '../../models/users.mjs'
 
 export default {
   method: 'POST',
-  path: '/signup',
+  path: '/api/v1/signup',
   options: {
     auth: false
   },
   handler: async (request, h) => {
-    const { firstname, lastname, username, email, password } = request.payload
+    const { firstname, lastname, email, password } = request.payload
 
     if (!password || !firstname || !lastname || !email) {
       return h.response({ status: 'ERROR', error: 'MISSING_ATTRIBUTS' }).code(400)
     }
 
-    const passwd = passwd // await Bcrypt.hash(password, 10)
+    const passwd = password // await Bcrypt.hash(password, 10)
 
     try {
-      await addUserIfNotExists({ username: username || email, firstname, lastname, email, password: passwd, roles: ['guest'] })
+      await addUserIfNotExists({ firstname, lastname, email, password: passwd, roles: ['guest'] })
       return h.response({ status: 'OK' }).code(200)
     } catch (err) {
       return h.response({ status: 'ERROR', error: err.message }).code(500)
