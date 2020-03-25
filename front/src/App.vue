2ff634b90bc1e7e33f982231bcbaa30e74f83c0d
Camille Moutte <mouttecam@gmail.com>
2020-03-25 12:21:34 +0100

    front: edit font-family

--- front/src/App.vue
+++ front/src/App.vue
@@ -60,7 +60,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Ubuntu,sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
