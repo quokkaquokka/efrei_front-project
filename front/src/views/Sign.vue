536cb0404998e1f248d5c642aa4ad4e99082928a
Théo <t.abdmeziem@gmail.com>
2020-04-22 00:58:08 +0200

    Front: login page style

--- front/src/views/Sign.vue
+++ front/src/views/Sign.vue
@@ -1,22 +1,27 @@
 <template>
-  <div id='sign'>
-    <nav>
-      <div class="nav nav-tabs" id="nav-tab" role="tablist">
-        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Se connecter</a>
-        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">S'inscrire</a>
-        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Mot de passe oublié ?</a>
+  <div id='card'>
+    <div id='sign'>
+      <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+          <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Se connecter</a>
+          <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">S'inscrire</a>
+          <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Mot de passe oublié ?</a>
+        </div>
+      </nav>
+      <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+          <Signin> </Signin>
+        </div>
+        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+          <Signup> </Signup>
+        </div>
+        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+          <ForgottenPassword></ForgottenPassword>
+        </div>
       </div>
-    </nav>
-    <div class="tab-content" id="nav-tabContent">
-      <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-        <Signin> </Signin>
-      </div>
-      <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-        <Signup> </Signup>
-      </div>
-      <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-        <ForgottenPassword></ForgottenPassword>
-      </div>
+    </div>
+    <div class="mx-auto mt-5 text-center">
+      <img src="@/assets/logo_big.png" height="250" alt="Logo"/>
     </div>
   </div>
 </template>
@@ -53,10 +58,21 @@ export default {
 <style scoped>
 #sign {
   width: 700px;
-  background-color: #ffffff;
+  background-color: #00A0C6;
   margin: auto;
   margin-top: 50px;
   padding: 20px;
+}
+
+#sign nav a, #sign div.tab-content {
+  background-color: #ffffff;
+}
+
+#card {
+  background-color: #00A0C6;
+  margin: auto;
+  margin-top: 50px;
+  padding: 80px;
 }
 
 #nav-tabContent{
